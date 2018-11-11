3ac0fa745e203501de3ad1514c9073be6b036d68
ema kolaric <emakolaric@icloud.com>
2018-11-11 20:55:47 +0100

    Verzija 1

--- Jabuke/MainWindow.xaml.cs
+++ Jabuke/MainWindow.xaml.cs
@@ -24,5 +24,58 @@ namespace Jabuke
         {
             InitializeComponent();
         }
+
+        List<Jabuka> Jabuke = new List<Jabuka>();
+        int sum = 0;
+
+        private void Zapamti(object sender, RoutedEventArgs e)
+        {
+            string kol = textBoxKolicina.Text;
+            Jabuka j1 = new Jabuka();
+            j1.boja = textBoxBoja.Text;
+            j1.kolicina = Convert.ToInt16(kol);
+            sum += j1.kolicina;
+            for (int x = 0; x < Jabuke.Count(); x++)
+            {
+                Jabuka j2 = Jabuke.ElementAt(x);
+                if (j2.boja == j1.boja)
+                {
+                    j2.kolicina += j1.kolicina;
+                }
+                else
+                {
+                    Jabuke.Add(j1);
+
+                }
+
+            }
+            textBoxBoja.Clear();
+            textBoxKolicina.Clear();
+
+        }
+
+        private void Izracunaj(object sender, RoutedEventArgs e)
+        {
+            textBoxZbrojKolicinaSvih.Text =sum.ToString() ;
+        }
+        
+
+        private void Pretrazi(object sender, RoutedEventArgs e)
+        {
+            int i=100000;
+            string min="";
+
+
+            foreach(Jabuka j1 in Jabuke)
+            {
+                if (j1.kolicina <i)
+                {
+                    min += j1.boja;
+                }
+            }
+
+               textBoxIspisNajmanjeJabukaBoje.Text= min;
+            
+        }
     }
 }
